a2361b3bfd0fec55012873b7324cdba0e0554cba
Zodicc <ikac.kikac@gmail.com>
2024-01-23 15:22:55 +0200

    first commit

---
+++ xv6-project-2023/kernel/spinlock.h
@@ -0,0 +1,12 @@
+#ifndef SPINLOCK_H
+#define SPINLOCK_H
+
+struct spinlock {
+  uint locked;       // Is the lock held?
+
+  // For debugging:
+  char *name;        // Name of lock.
+  struct cpu *cpu;   // The cpu holding the lock.
+};
+
+#endif // SPINLOCK_H
